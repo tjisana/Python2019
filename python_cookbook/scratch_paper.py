23b3a3f92b1aab83f18849d64255d2e52b5c1660
Tjisana Kerr <tjisanakerr@tjisanas-mbp.myfiosgateway.com>
2022-05-22 21:53:25 -0400

    clean up repo

--- python_cookbook/scratch_paper.py
+++ python_cookbook/scratch_paper.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+
 def search(lines, pattern, history=5):
     """search function yielding found pattern plus previous 5 lines"""
     previous_lines = deque(maxlen=history)
@@ -8,10 +9,11 @@ def search(lines, pattern, history=5):
             yield line, previous_lines
         previous_lines.append(line)
 
+
 if __name__ == "__main__":
     with open('somefile.txt') as f:
         for line, prev in search(f, 'python', 5):
             for pline in prev:
                 print(pline, end='')
             print(line, end='')
-            print('-'* 20)
+            print('-' * 20)
